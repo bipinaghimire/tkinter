1a777d9e34030da325fc1201613c06edf6829532
user <bipinaghimire6@gmail.com>
2021-08-02 12:12:55 +0545

    check button

--- image/check button.py
+++ image/check button.py
@@ -3,7 +3,8 @@ root = Tk()
 var = StringVar()
 def show():
     myLabel = Label(root, text= var.get()).pack()
-checkbutton = Checkbutton(root, text="check this box", variable=var, onvalue= "On", offvalue="Off")
+checkbutton = Checkbutton(root, text="check this box", variable=var, onvalue= "Active", offvalue="Unactive")
+#automatic selection will be removed.
 checkbutton.deselect()
 checkbutton.pack()
 
