4d57ff4428638a73d463ece1c296814ce963c309
user <bipinaghimire6@gmail.com>
2021-08-02 10:56:14 +0545

    message box

--- image/messagebox.py
+++ image/messagebox.py
@@ -6,7 +6,7 @@ root.title('Message Box')
 def pop():
     response=messagebox.askyesno("This is my popup","hello everyone")
     Label(root, text=response).pack()
-    if response==1:
+    if response ==1:
         Label(root, text="Clicked Yes").pack()
     else:
         Label(root,text="Clicked No").pack()
